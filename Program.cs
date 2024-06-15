1d1fb72516b9587763468fcdd748fe37f4ebcc21
myoung <galt_69@hotmail.com>
2024-06-14 21:56:08 -0400

    Add a sub-command and options for it.

--- Program.cs
+++ Program.cs
@@ -10,21 +10,50 @@ class Program
             name: "--file",
             description: "The file to read and display on the console.");
 
-        var rootCommand = new RootCommand("Sample app for System.CommandLine");
-        rootCommand.AddOption(fileOption);
+        var delayOption = new Option<int>(
+            name: "--delay",
+            description: "Delay between lines, specified as milliseconds per character in a line.",
+            getDefaultValue: () => 42);
 
-        rootCommand.SetHandler((file) => 
-            { 
-                ReadFile(file!); 
-            },
-            fileOption);
+        var fgcolorOption = new Option<ConsoleColor>(
+            name: "--fgcolor",
+            description: "Foreground color of text displayed on the console.",
+            getDefaultValue: () => ConsoleColor.White);
+
+        var lightModeOption = new Option<bool>(
+            name: "--light-mode",
+            description: "Background color of text displayed on the console: default is black, light mode is white.");
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+
+        var readCommand = new Command("read", "Read and display the file.")
+            {
+                fileOption,
+                delayOption,
+                fgcolorOption,
+                lightModeOption
+            };
+        rootCommand.AddCommand(readCommand);
+
+        readCommand.SetHandler(async (file, delay, fgcolor, lightMode) =>
+        {
+            await ReadFile(file!, delay, fgcolor, lightMode);
+        },
+        fileOption, delayOption, fgcolorOption, lightModeOption);
 
         return await rootCommand.InvokeAsync(args);
     }
 
-    static void ReadFile(FileInfo file)
+    internal static async Task ReadFile(
+        FileInfo file, int delay, ConsoleColor fgColor, bool lightMode)
     {
-        File.ReadLines(file.FullName).ToList()
-            .ForEach(line => Console.WriteLine(line));
+        Console.BackgroundColor = lightMode ? ConsoleColor.DarkYellow : ConsoleColor.Black;
+        Console.ForegroundColor = fgColor;
+        List<string> lines = File.ReadLines(file.FullName).ToList();
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+            await Task.Delay(delay * line.Length);
+        };
     }
 }
