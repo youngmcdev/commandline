fb0e8a35ee96f5ca9cd79d8eb1885e614a2b0268
myoung <galt_69@hotmail.com>
2024-06-14 21:06:51 -0400

    Make program interactive.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.Read();
     }
 }
